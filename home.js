c6e8a6591f9165d2a57def9f8ee0bc05e2464e6e
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-07-13 16:33:24 +0530

    conditions applied for board name

--- home.js
+++ home.js
@@ -36,34 +36,43 @@ function toggleMenu() {
 var idName = 1;
 
 function addBoard(id) {
-  idName++;
-  var listId = "projectList_" + idName;
-  var boardId = "board_" + idName;
-  var templateBlock =
-    '<section class="board-block" id=' +
-    boardId +
-    ">" +
-    "<div class = 'section-div'>" +
-    id.value +
-    '<button class = "remove-btn" onclick="removeCards(' +
-    listId +
-    ')">Remove Card</button></div>' +
-    '<div class="project-block" id= ' +
-    listId +
-    ">" +
-    "</div>" +
-    "</section>";
-  document.getElementById("boardBlockList").innerHTML += templateBlock;
-  showProjects(listId);
-  document.getElementById("menuList").innerHTML +=
-    '<li class= "menuListItems" onclick = "loadMenu(' +
-    boardId +
-    ')">' +
-    id.value +
-    "</li>" +
-    "<hr>";
+  if (id.value.length < 5) {
+    alert("Board name can't be less than five characters!");
+    id.value = id.defaultValue;
+    return;
+  } else if (id.value == null || id.value == "") {
+    alert("Board name can't be empty!");
+    return;
+  } else {
+    idName++;
+    var listId = "projectList_" + idName;
+    var boardId = "board_" + idName;
+    var templateBlock =
+      '<section class="board-block" id=' +
+      boardId +
+      ">" +
+      "<div class = 'section-div'>" +
+      id.value +
+      '<button id = "remove-btn" onclick="removeCards(' +
+      listId +
+      ')">Remove Card</button></div>' +
+      '<div class="project-block" id= ' +
+      listId +
+      ">" +
+      "</div>" +
+      "</section>";
+    document.getElementById("boardBlockList").innerHTML += templateBlock;
+    showProjects(listId);
+    document.getElementById("menuList").innerHTML +=
+      '<li class= "menuListItems" onclick = "loadMenu(' +
+      boardId +
+      ')">' +
+      id.value +
+      "</li>" +
+      "<hr>";
 
-  document.getElementById(id.id).value = "";
+    document.getElementById(id.id).value = "";
+  }
 }
 
 function loadMenu(element) {
